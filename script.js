aa67c9585586b807958cf2cb685035b7a06a8869
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-29 20:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-// Complete the js code
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
+	this.make = make;
+	this.model = model;
 }
 
-Car.prototype.getMakeModel=function {
-
+Car.prototype.getMakeModel = function() {
 	return `${this.make} ${this.model}`;	
 }
 
-
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.topSpeed=topSpeed;
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
 }
-SportsCar.prototype=Object.create(Car.prototype);
-SportsCar.prototype.constructor=SportsCar;
 
-SportsCar.prototype.getTopSpeed=function() {
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
 	return this.topSpeed;
 }
 
